24d1de3ca1ad8347b81d2b098e09e99160ea1d9b
Cyuan <34447298+dec880126@users.noreply.github.com>
2021-06-14 01:50:22 +0800

    Update Array_Partition_I.py

--- Array_Partition_I.py
+++ Array_Partition_I.py
@@ -8,19 +8,26 @@
 # 3. (1, 2), (3, 4) -> min(1, 2) + min(3, 4) = 1 + 3 = 4
 # So the maximum possible sum is 4.
 
-# Original complement: Runtime = 296 ms ; Memory = 16.7 MB
+# Better complement: Runtime = 268 ms ; Memory = 17 MB
 class Solution:
-    def arrayPairSum(self, nums: list[int]) -> int:
-        sum = 0
-        nums.sort()
-        nums = list(list_spilt(nums, 2))
-        for i in range(len(nums)):
-            sum += min(nums[i][0], nums[i][1])
-        return sum
+    def arrayPairSum(self, nums: List[int]) -> int:
+        nums = sorted(nums, reverse = True)
+        return sum(nums[i] for i in range(1, len(nums), 2))
 
-def list_spilt(list, n):
-    for i in range(0, len(list), n):
-        yield list[i:i+n]
+# Original complement: Runtime = 296 ms ; Memory = 16.7 MB
+# class Solution:
+#     def arrayPairSum(self, nums: list[int]) -> int:
+#         sum = 0
+#         nums.sort()
+#         nums = list(list_spilt(nums, 2))
+#         for i in range(len(nums)):
+#             sum += min(nums[i][0], nums[i][1])
+#         return sum
+
+# def list_spilt(list, n):
+#     for i in range(0, len(list), n):
+#         yield list[i:i+n]
+
 #Paramater
 nums = [6,2,6,5,1,2]
 
